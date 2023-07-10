f36eae22e7940b1df067d6ed0e4df3ca818f9df3
refigo <42.4.mgo@gmail.com>
2023-07-10 16:00:27 +0900

    Add example for servo motor with button

--- arduino_ws/servo_with_button/servo_with_button.ino
+++ arduino_ws/servo_with_button/servo_with_button.ino
@@ -36,3 +36,44 @@ void loop() {
     delay(100);
   }
 }
+
+
+// Example for servo motor with button
+/*
+#include <Servo.h>
+int pos = 0;
+int push_btn = 2;
+bool flag = 0;
+bool dir = 0;
+Servo servo;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(push_btn, INPUT);
+  servo.attach(9);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  bool incoming_data = digitalRead(push_btn);
+  if(incoming_data==HIGH & flag==0){
+    Serial.println("Button is pressed.");
+    flag = 1;
+    // rotate
+    if(dir == 0) {
+      pos += 10;
+    }
+    else {
+      pos -= 10;
+    }
+    servo.write(pos);
+    // check if dir need to be changed
+    if(pos >= 180 || pos <= 0) {
+      dir = !dir;
+    }
+  }
+  if(incoming_data==LOW & flag==1){
+    Serial.println("---");
+    flag = 0;
+  }
+}
+*/
